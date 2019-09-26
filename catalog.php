6d45b8129b6aaaf42ddfe4b6c415d9525d3405d1
Julia <julia.fedyaeva@mail.ru>
2019-09-26 17:02:20 +0300

    Конечный код для 3 задачи

--- catalog.php
+++ catalog.php
@@ -1,9 +1,60 @@
 <?php
 require("components/header.php");
 
-$photos = ["Диваны", "Люстры", "Коврики", "Столы", "Кошачьи радости"];
-$index = 0;
-$num = count($items);
+$param = [
+  0 => [
+  'photo' => 'img/item-mseyulida.jpg',
+  'name' => 'Лампа Мсеюлида',
+  'price' => 4000,
+  'height' => 120,
+  'width' => 90,
+  'weight' => 3,
+  'colors' => ["Красный", "Зелёный", "Розовый", "Леопардовый"]
+],
+
+1 => [
+  'photo' => 'img/item-nnulm.jpg',
+  'name' => 'Рабочий стол Ннулм',
+  'price' => 5000,
+  'height' => 90,
+  'width' => 50,
+  'weight' => 8,
+  'colors' => ["Синий", "Желтый", "Непонятный"]
+
+], 
+
+2 => [
+  'photo' => 'img/item-tre.jpg',
+  'name' => 'Набор мебели Тре',
+  'price' => 20000,
+  'height' => '-',
+  'width' => '-',
+  'weight' => 20,
+  'colors' => ["Лиловый", "Пятнистый"]
+], 
+
+3=> [
+  'photo' => 'img/item-blempere.jpg',
+  'name' => 'Кресло Бадета',
+  'price' => 500,
+  'height' => 100,
+  'width' => 520,
+  'weight' => 30,
+  'colors' => ["Один цвет"]
+],
+
+4 => [
+  'photo' => 'img/item-nmyao.jpg',
+  'name' => 'Подушка Нмяо',
+  'price' => 1500,
+  'height' => 320,
+  'width' => 200,
+  'weight' => 0.5,
+  'colors' => ["Бордовый", "Черепаший", "Тигровый"]
+];
+
+$index = 1;
+$number = count($param);
 
 require("components/products_list.php");
 require("components/footer.php");
--- components/products_list.php
+++ components/products_list.php
@@ -1,62 +1,24 @@
 <section class="catalog">
   <div class="container">
     <h1 class="catalog-title">Каталог товаров</h1>
-  <!-- Здесь или в другом месте список категорий -->
-    <ul>
-      <?php while ($index < $num): ?>
-      <li><a href="#"><?=$categories[$index]; ?></a></li>
-      <?php $index ++; ?>
-      <?php endwhile; ?>
-    </ul>
+
     <ul class="products-list">
+      <?php while ($index < $number): ?>
       <li>
         <a class="product-card" href="product.php?product_id=1">
-          <h3>Мсеюлида</h3>
-          <p>лампа напольная</p>
-          <span class="price"><del>289₽</del> 288₽</span>
-          <img src="img/item-mseyulida.jpg" width="156" height="120" alt="Напольная лампа «Мсеюлида»">
+          <h3><?=$param[index]['name']; ?></h3>
+          <span class="price"><?=$param[index]['price']; ?></span>
+          <img src="<?=$param[index]['photo']; ?>" width="156" height="120" alt="<?=$param[index]['name']; ?>">
+          <ul class="parametrs">
+            <li><?=$param[index]['height']; ?></li>
+            <li><?=$param[index]['width']; ?></li>
+            <li><?=$param[index]['weight']; ?></li>
+            <li><?=$param[index]['color']; ?></li>
+          </ul>
         </a>
       </li>
-      <li>
-        <a class="product-card product-card-new" href="product.php?product_id=2">
-          <h3>Рмаериби</h3>
-          <p>диван трёхместный</p>
-          <span class="price">5870₽</span>
-          <img src="img/item-rmaeribi.jpg" width="156" height="120" alt="Трёхместный диван «Рмаериби»">
-        </a>
-      </li>
-      <li>
-        <a class="product-card" href="product.php?product_id=3">
-          <h3>Блетуб</h3>
-          <p>люстра потолочная</p>
-          <span class="price">1360₽</span>
-          <img src="img/item-bletub.jpg" width="156" height="120" alt="Потолочная люстра «Блетуб»">
-        </a>
-      </li>
-      <li>
-        <a class="product-card" href="product.php?product_id=4">
-          <h3>Ннулм</h3>
-          <p>рабочий стол</p>
-          <span class="price">6832₽</span>
-          <img src="img/item-nnulm.jpg" width="156" height="120" alt="Компьютерный стол «Ннулм»">
-        </a>
-      </li>
-      <li>
-        <a class="product-card" href="product.php?product_id=5">
-          <h3>Асусмер</h3>
-          <p>подвесная кровать</p>
-          <span class="price"><del>21320₽</del> 19320₽</span>
-          <img src="img/item-asusmer.jpg" width="156" height="120" alt="Подвесная кровать «Асусмер»">
-        </a>
-      </li>
-      <li>
-        <a class="product-card product-card-new" href="product.php?product_id=6">
-          <h3>Тре</h3>
-          <p>набор мебели</p>
-          <span class="price">32560₽</span>
-          <img src="img/item-tre.jpg" width="156" height="120" alt="Набор мебели «Тре»">
-        </a>
-      </li>
+      <?php $index ++; ?>
+      <?php endwhile; ?>
     </ul>
   </div>
 </section>
